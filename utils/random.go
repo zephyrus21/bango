37f58a6e32d5cb7f85c6875c3c6ba9deb32ee966
zephyrus21 <pandey.piyus@gmail.com>
2023-08-05 21:03:56 +0530

    added transfer api

--- utils/random.go
+++ utils/random.go
@@ -38,7 +38,7 @@ func RandomMoney() int64 {
 
 // RandomCurrency generates a random currency code
 func RandomCurrency() string {
-	currencies := []string{"USD", "EUR", "INR"}
+	currencies := []string{USD, EUR, INR}
 	n := len(currencies)
 	return currencies[rand.Intn(n)]
 }
